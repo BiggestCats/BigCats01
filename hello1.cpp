ee74176ae94ea3b2235a9fd34acc15db66ff482f
BiggestCats <peripheralvision120@gmail.com>
2024-03-27 11:50:44 -0700

    Typo fix

--- hello1.cpp
+++ hello1.cpp
@@ -5,7 +5,8 @@ int main()
 {
     cout << "Hello world!\n";
     cout << "The world is a motherfucker.\n";
+    cout << "9 billion dead cops\n";
     cout << "kill them all\n";
-    cout << "I am jumpja stick\n";
+    cout << "I am jumja stick\n";
     return 0;
 }
