9fb993a50d5f2eed6b6db55bd4d5afea5f9f9a14
BiggestCats <peripheralvision120@gmail.com>
2024-03-27 11:45:04 -0700

    Made hello world ruder

--- hello1.cpp
+++ hello1.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello world!";
+    cout << "Hello world!\n";
+    cout << "The world is a motherfucker.\n";
+    cout << "kill them all\n";
+    cout << "I am jumpja stick\n";
     return 0;
 }
